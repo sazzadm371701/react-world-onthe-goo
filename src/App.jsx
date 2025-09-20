7b136ca3d78990891b9fcc416fa008246cb8116c
sazzadm371701 <sazzadhosen3717@gmail.com>
2025-09-21 02:35:25 +0600

    Loaded the countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Suspense } from "react";
 import "./App.css";
 import Countries from "./components/Countries/Countries";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all"
+).then((res) => res.json());
 
 function App() {
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Nadir vai loading...</p>}>
+        <Countries countriesPromise={countriesPromise}> </Countries>
+      </Suspense>
     </>
   );
 }
--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { use } from "react";
 
-const Countries = () => {
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  console.log(countries);
   return (
     <div>
-      <h1>In the Countries</h1>
+      <h1>In the Countries: {countries.length}</h1>
     </div>
   );
 };
